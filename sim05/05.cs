5376ea9d91e25c7a719a38a834a98de08712460a
Your Snezhana <volhonskaya_snez@mail.ru>
2023-05-27 19:40:48 +0400

    05

---
+++ sim05/05.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимае на входе два числа и проверяет, является ли одно число квадратом другого.
+// 5,25- да 
+//-4,16 -да
+// 25,5 -да
+// 8,9 -нет
+Console.WriteLine("Ввидите целое число А: ");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Ввидите целое число B: ");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+int doubleNum1= num1* num2;
+int doubleNum2=num2 * num1;
+if (doubleNum1== num2 || doubleNum2==num1)
+System.Console.WriteLine("Да");
+else
+System.Console.WriteLine("Нет");
+
