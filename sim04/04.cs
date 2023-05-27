bc343a6f30e7ab1d6ef5e49011426896eb4498ae
Your Snezhana <volhonskaya_snez@mail.ru>
2023-05-27 19:39:46 +0400

    04

---
+++ sim04/04.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу,которая принимает на входе число и проверяет ,кратно ли оно одновременно 7 и 23.
+// 14->нет
+//46-> нет
+//161->да
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num% 7==0 && num%23==0)
+System.Console.WriteLine($"{num}оно кратно 7 и 23");
+else 
+System.Console.WriteLine($"{num} оно не кратно 7 и 23");
